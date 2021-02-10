cc50843fcdc5d58e4db506375de3ce314b09f38e
WitholdP <w.piecewicz@gmail.com>
2021-02-10 11:58:06 +0100

    full module of logging in working on the home page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from connection import connect
 from models import User
 
@@ -8,7 +8,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    message = None
+    loged_in_check = False
+    connection = connect()
+    cursor = connection.cursor()
+    check_for_login = User.login_check(cursor)
+    if check_for_login:
+        loged_in_check = True
+
+    return render_template('index.html', loged_in_check = loged_in_check, message = message)
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -20,10 +29,29 @@ def register():
         adding_user = new_user.add_user(cursor)
         if adding_user == 'user_added':
             message = 'User added to data base'
-
     connection.close()
 
     return render_template('register.html', message=message)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    message = None
+    connection = connect()
+    cursor = connection.cursor()
+    if request.method == 'POST':
+        login_check = User.log_in(request.form['username'], request.form['password'], cursor)
+        if login_check == 'no user':
+            message = 'Wrong username bitch!'
+        elif login_check == 'wrong password':
+            message = 'Wrong password bitch!'
+        elif login_check == 'loged in':
+            message = 'You have logged in'
+            return redirect('/')
+    connection.close()
+
+    return render_template('login.html', message = message)
+
 
 @app.route('/users')
 def user():
--- models.py
+++ models.py
@@ -20,6 +20,38 @@ class User(object):
             return 'user_added'
         return False
 
+    @staticmethod
+    def log_in(username, password, cursor):
+        """Login function:
+            1 - it checkes if username exists in data database
+            2 - if password matches username it sets value of loged_in parameter to True, for this user"""
+
+        sql = f"""SELECT * FROM users WHERE username = '{username}' ;"""
+        cursor.execute(sql)
+        check_if_exists = cursor.fetchone()
+
+        if check_if_exists:
+            if password == check_if_exists[4]:
+                log_in_sql = f"""UPDATE users SET loged_in = True WHERE username = '{username}';"""
+                cursor.execute(log_in_sql)
+                return 'loged in'
+            else:
+                return 'wrong password'
+        else:
+            return 'no user'
+
+    @staticmethod
+    def login_check(cursor):
+        """ Function will check if any of the users has atribute loged_in set to True.
+        If yes it will change teh contect on the page. Some pages will not be available
+        for users that are not logged in."""
+
+        sql = """SELECT * FROM users WHERE loged_in = True ;"""
+        cursor.execute(sql)
+        check = cursor.fetchone()
+        if check:
+            return True
+
 
     def __str__(self):
         return self.username
